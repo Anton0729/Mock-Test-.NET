9a5470abd60ddd54d2f38160802a00b8979d1b9c
Anton Skazko <65301379+Anton0729@users.noreply.github.com>
2024-04-30 06:08:20 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,62 +1,163 @@
 /*
-Write one program, that reads a series of integer numbers from the keyboard and performs the tasks de-
-scribed below. Each number can be entered as a separate line of text (with the ENTER key pressed at the
-end). The entering of numbers should be stopped when the user enters a non-number, e.g. an empty line
-or a text like "stop".
+Console.WriteLine("Enter lines of text. Enter an empty line to stop.");
+List<string> lines = new List<string>();
 
-After reading the numbers the program should display three lines of text containing information de-
-scribed below. Each of the three points • should result in one line of text. Printing of the results must
-occur after all the numbers have been read from the keyboard.
+while (true)
+{
+    string inputString = Console.ReadLine();
 
-Note: You can use course materials and web search during the test, but do not use any code- gen-
-erating tool (Copilot, ChatGPT, etc.). Using such tools results in getting 0 points from the test.
-
-how many of the entered numbers are not divisible by 2 nor by 5?
-
-how many of the entered numbers are larger than 15 and at the same time divisible
-by 9?
-
-calculate the average of the two values mentioned above.
-*/
-using System;
-
-class Program {
-    static void Main(string[] args){
-        int totalCount = 0;
-        int countTask1 = 0;
-        int countTask2 = 0;
-        int sum = 0;
-
-        Console.WriteLine("Enter numbers or type non-number to stop");
-        while (true)
-        {
-            string input = Console.ReadLine();
-            if (int.TryParse(input, out int number))
-            {
-                totalCount++;
-
-                if (number % 2 != 0 && number % 5 != 0)
-                {
-                    countTask1++;
-                }
-
-                if (number > 15 && number % 9 == 0)
-                {
-                    countTask2++;
-                    sum += number;
-                }
-
-            }
-            else {
-                break;
-            }
-        }
-
-        double average = (double)(countTask1 + countTask2) / 2;
-
-        Console.WriteLine($"Numbers not divisible by 2 or 5: {countTask1}");
-        Console.WriteLine($"Numbers larger than 15 and divisible by 9: {countTask2}");
-
-        Console.WriteLine($"Average: {average}");
+    if (string.IsNullOrEmpty(inputString))
+    {
+        break;
     }
+
+    lines.Add(inputString);
 }
+
+*/
+
+
+
+/*
+foreach (var line in lines)
+{
+*/
+    /*
+    Console.WriteLine("Task 1");
+    string reversedString = new string(line.Reverse().ToArray());
+
+    if (line == reversedString)
+    {
+        Console.WriteLine("Yes, it is a palindrome.");
+    }
+    else
+    {
+        Console.WriteLine("No, it is not a palindrome.");
+    }
+    */
+
+
+    /*
+    // NOT SHURE HOW IT WORKS
+    Console.WriteLine("Task 2");
+    // “ABCDEFGH” you should print: “BADCFEHG”
+    string input = Console.ReadLine();
+    char[] chars = input.ToCharArray();
+    for (int i = 0; i < chars.Length; i += 2)
+    {
+        char temp = chars[i];
+        chars[i] = chars[i + 1];
+        chars[i + 1] = temp;
+
+    }
+    Console.WriteLine(chars);
+    */
+
+
+    /*
+    Console.WriteLine("Task 3");
+    int sumOfDigits = 0;
+    foreach (char i in line)
+    {
+        if (char.IsDigit(i))
+        {
+            sumOfDigits += int.Parse(i.ToString());
+        }
+    }
+    Console.WriteLine($"Sum of digits: {sumOfDigits}");
+    */
+
+
+    /*
+    Console.WriteLine("Task 4");
+    string input = Console.ReadLine();
+
+    int sumOfDigits = 0;
+    int sumOfChars = 0;
+
+    foreach (char i in input) {
+        if (char.IsDigit(i)) {
+            sumOfDigits++;
+        }
+        if (char.IsLetter(i)) {
+            sumOfChars++;
+        }
+    } 
+
+    if (sumOfChars > sumOfDigits)
+    {
+        Console.WriteLine("Letters more than digits");
+    }
+    else {
+        Console.WriteLine("No, letters less than digits");
+    }
+    */
+
+    /*
+    Console.WriteLine("Task 5");
+    string input = Console.ReadLine();
+
+    int sumOfUpper = 0;
+    int sumOfLower = 0;
+
+    foreach (char i in input) {
+        if (char.IsUpper(i)) {
+            sumOfUpper++;
+        }
+        if (char.IsLower(i)) {
+            sumOfLower++;
+        }
+    } 
+
+    if (sumOfUpper > sumOfLower)
+    {
+        Console.WriteLine("Upper case more than lower case characters");
+    }
+    else {
+        Console.WriteLine("No, upper case less than lower case characters");
+    }
+    */
+
+
+    /*
+    Console.WriteLine("Task 7");
+    
+    Print each line of text in the following way: if a character is a ‘-‘ (minus sign) skip the next
+    character, if a character is a ‘+’ (plus sign) print the next character twice. Print all the other
+    characters normally. For example, if the input is: “-A+BC-+DDD+-“ you should print:
+    “BBCDDD--" (note that there are two minus signs at the end in the output). 
+    
+
+    string input = Console.ReadLine();
+    string new_string = "";
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == '-')
+        {
+            i++;
+            continue;
+        }
+        else if (input[i] == '+')
+        {;
+            new_string += input[i + 1];
+            new_string += input[i + 1];
+            i++; 
+        }
+        else
+        {
+            new_string += input[i];
+        }
+    }
+
+    Console.WriteLine($"Enterd string {input}. Result {new_string}");
+    */
+
+
+    Console.WriteLine("Task 8");
+
+
+
+/*    
+}
+*/
